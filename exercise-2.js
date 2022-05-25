a7711946d90488235ddce17f8072de15b9cda1b2
gabriele_simone <gabriele.simone1@studenti.unisalento.it>
2022-05-25 17:26:35 +0200

    first commit

--- exercise-2.js
+++ exercise-2.js
@@ -1,7 +1,6 @@
 const order = {};
 
-console.log(order?.customer?.address?.city); 
 
-if (order && order.customer && order.customer.address && !order.customer.address.city) {
+if (!order?.customer?.address?.city) {
   console.log('City is required');
 }
